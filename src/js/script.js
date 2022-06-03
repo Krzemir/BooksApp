1e226f0f73e9bb961595bab5a707c05032f25d70
Krzysztof Mirończuk <krzysztof.mironczuk@socart.media>
2022-06-03 14:34:05 +0200

    render fn - dblclick problem

--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,53 @@
 {
   'use strict';
   
-  const books = dataSource.books;
-  const booksContainer = document.querySelector('.books-list');
-  
+  const booksData = dataSource.books;
+
+  const dom = {
+    booksContainer: document.querySelector('.books-list'),
+   
+  };
   const templates = {
     bookList: Handlebars.compile(document.querySelector('#template-book').innerHTML)
   };
+
+  const favoriteBooks = [];
   
   
   
   function render(){
     const thisBook = this;
     // console.log(thisBook);
-    for(const bookId of books){
+    for(const bookId of booksData){
       //console.log(bookId);
   
       const generatedHTML = templates.bookList(bookId);
   
       thisBook.element = utils.createDOMFromHTML(generatedHTML);
-      booksContainer.appendChild(thisBook.element);
+      dom.booksContainer.appendChild(thisBook.element);
     }
   
   }
   render();
+
+  function initActions(){
+    const covers = dom.booksContainer.querySelectorAll('a');
+
+    for (const cover of covers){
+      console.log(cover);
+      const bookId = cover.getAttribute('data-id');
+      console.log(bookId);
+
+      cover.addEventListener('dblclick', function (event) {
+        event.preventDefault();
+        console.log('clicked');
+        favoriteBooks.push(bookId);
+        console.log(favoriteBooks);
+        cover.classList.add('favorite');
+      });
+    }
+    
+
+  }  initActions();
 }
   
